b05e3f61afc4205ee374ddf0296ef56a42e35701
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-22 10:48:30 -0800

    Hook button up to move swimmers in a random direction on each click

--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -1,6 +1,7 @@
 
 $('body').on('keydown', (event) => {
   var arrowPress = event.key.match(/Arrow(Up|Down|Left|Right)/);
+  console.log(arrowPress);
   if (arrowPress) {
     var direction = arrowPress[1];
     SwimTeam.move(direction.toLowerCase());
