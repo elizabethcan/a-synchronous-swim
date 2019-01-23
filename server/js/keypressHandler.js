2073c72f9c019e376f95302365e4c3652ae7327f
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-22 16:37:59 -0800

    Add functionality to queue up movements on the server side to be executed one by one by button click on the client side

--- server/js/keypressHandler.js
+++ server/js/keypressHandler.js
@@ -1,5 +1,7 @@
 const _ = require('underscore');
 const keypress = require('keypress');
+const enqueue = require('./messageQueue').enqueue;
+const dequeue = require('./messageQueue').dequeue;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Utility Function ///////////////////////////////////////////////////////////
@@ -37,6 +39,7 @@ module.exports.initialize = () => {
     // check to see if the keypress itself is a valid message
     if (isValidMessage(key.name)) {
       console.log(`Message received: ${key.name}`);
+      enqueue(key.name);
       return; // don't do any more processing on this key
     }
     
@@ -67,3 +70,5 @@ if (process.stdin.setRawMode) {
   // configure stdin for raw mode, if possible
   process.stdin.setRawMode(true);
 }
+
+exports.validMessages = validMessages;
--- server/js/request-handler.js
+++ server/js/request-handler.js
@@ -1,12 +1,20 @@
 const headers = require('./cors');
-const directions = ['left', 'right', 'up', 'down']
+// const directions = ['left', 'right', 'up', 'down']
+const directions = require('./keypressHandler').validMessages;
+const dequeue = require('./messageQueue').dequeue;
 
 var requestHandler = function(request, response) {
-  var randomDir = directions[Math.floor(Math.random() * directions.length)]
-  if(request.method === 'GET') {
-    console.log('randomDir: ' + randomDir);
+  // var randomDir = directions[Math.floor(Math.random() * directions.length)]
+  // if(request.method === 'GET') {
+  //   console.log('randomDir: ' + randomDir);
+  //   response.writeHead(200, headers);
+  //   response.end(randomDir);
+  // }
+  if (request.method === 'GET') {
+    var move = dequeue();
+    console.log(move);
     response.writeHead(200, headers);
-    response.end(randomDir);
+    response.end(move);
   }
 }
 
